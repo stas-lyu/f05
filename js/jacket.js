65eae200ced9b79c3a1a6b53841379c914e04f6c
stas-lyu <61148250+stas-lyu@users.noreply.github.com>
2021-04-27 19:16:22 +0300

    Update jacket.js

--- js/jacket.js
+++ js/jacket.js
@@ -1,11 +1,14 @@
 let jacket = document.querySelector('.jacket')
+let jacketProd = document.querySelector('.jacket_prod')
+
 fetch('catalog.json')
     .then(response => response.json())
     .then(json => {
         json.jacket.forEach(item => {
+
             jacket.innerHTML += `
             <div class="col s12 m4">
-                 <div class="jacket_prod">
+            <div class="jacket_prod">
                     <div class="jacket_name">Бренд : ${item.name}</div>
                     <div class="carousel">
                     </div>
@@ -18,30 +21,51 @@ fetch('catalog.json')
                     <div class="jacket_color">Цвета в наличии: ${item.color}</div>
                     <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Перейти к покупке</a>
                     </div>
-                        </div>
+                    </div>
         `
-            let jacketProd = document.querySelectorAll(".jacket_prod")
-
-            jacketProd.forEach((item) => {
-                item.addEventListener("click", (item) => {
-                    let price = item.querySelector('.newprice');
-                    console.log(price);
-                })
-            })
-
             postImg(item.imageUrls)
         });
+        $('.modal').modal();
+        let jacketProd = document.querySelectorAll(".jacket_prod")
+        let modalContent = document.querySelector(".modal-content");
+        jacketProd.forEach((item) => {
+            item.addEventListener("click", () => {
+                let jacket_name = item.querySelector(".jacket_name").innerText;
+                let carousel = item.querySelector(".carousel").innerHTML;
+                let oldPrice = item.querySelector(".oldPrice").innerText;
+                let newprice = item.querySelector(".newprice").innerText;
+                let categories = item.querySelector(".jacket_categories").innerHTML;
+                let quantity = item.querySelector(".jacket_quantity").innerText;
+                let color = item.querySelector(".jacket_color").innerText;
+                modalContent.innerHTML += `
+                <div class="jacket_name">Бренд : ${jacket_name}</div>
+                <img src="${carousel}" alt="">
+            <div>
+                    <div class="jacket_price">Цена :
+                    <div class="oldPrice">${oldPrice}</div>
+                    <div class="newprice">${newprice}</div>
+                    <div class="jacket_categories">Категория:${categories}</div>
+                    <div class="jacket_quantity">Количество:${quantity}</div>
+                    <div class="jacket_color">Цвета в наличии: ${color}</div>
+                    </div>
+                    
+                `
+                console.log({ jacket_name }, { oldPrice }, { newprice }, carousel)
+            })
+        })
         // <div class="jacket_description">Описание: ${item.description}</div>
     }).then(() => {
-    let slick = $('.carousel');
+        let slick = $('.carousel');
 
-    slick.slick({
-        lazyLoad: 'progressive',
-        dots: true,
-        arrows: false,
-    });
-})
-
+        slick.slick({
+            lazyLoad: 'progressive',
+            dots: true,
+            arrows: false,
+        });
+    })
+{/* <div class="jacket_categories">Категория:${item.categories}</div>
+                    <div class="jacket_quantity">Количество:${item.quantity}</div>
+                    <div class="jacket_color">Цвета в наличии: ${item.color}</div> */}
 
 
 let postImg = (array) => {
