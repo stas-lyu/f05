32f3b337146e1a1ffe6355cbff851d0014e5a0ab
Bixapb <Bixapb@gmail.com>
2021-04-28 20:35:49 +0300

    shirt

--- js/shirt.js
+++ js/shirt.js
@@ -2,38 +2,101 @@ $(document).ready(function() {
     fetch('catalog.json')
         .then(response => response.json())
         .then(json => {
+            console.log(json)
+
             let row = document.querySelector('.shirt-box')
             json.shirt.forEach(element => {
                 row.innerHTML += `
-                <div class=" col s12 m4 ">
-                <div class="shirt_product">
-                <p class="shirt_brand">Бренд : ${element.brand}</p>
+                <div class=" col s12 m4 shirt_product">
+
                 <div class="carusel-shirt">
-                </div>
-            <p class="shirt_name">Названые: ${element.name}</p>
-            <div class="price"> Цена :
-                <div class="oldPrice">${element.previousPrice} ₴</div>
-                <div class="newPrice">${element.currentPrice} ₴</div>
-            </div>
-            <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
-            </div>
-            </div>
-        `
+                     </div>
+
+                     <p class="shirt_name">Названые: ${element.name}</p>
+                    <div class="price"> Цена :
+                     <span class="oldPrice">${element.previousPrice} ₴</span>
+                    <span class="newPrice">${element.currentPrice} ₴</span>
+                    
+                    </div>
+                    <div class="modalInfo">
+                    <div class="shirt-brand">${element.brand}</div>
+                    <div class="shirt-color">${element.color} </div>
+                    <div class="shirt-discription">${element.description}</div>
+                    </div>
+                    <div class="orderBox">
+                    <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Заказать</a>
+                    <div class="like"><i class="fas addCart fa-cart-plus"></i></div>
+                    </div>
+                     </div>
+              
+                `
                 postImg(element.imageUrls)
+
+
             });
         }).then(() => {
-        let slick = $('.carusel-shirt');
-        slick.slick({
-            dots: true,
-            arrows: false,
-            lazyLoad: 'progressive',
-            slidesToShow: 1,
-        });
-    })
+            const modalShirt = document.querySelector('.modal-content');
+            let modalCard = document.querySelectorAll('.shirt_product');
+
+            modalCard.forEach(el => {
+                el.addEventListener('click', () => {
+                    let shirtBrand = el.querySelector('.shirt-brand');
+                    let shirtColor = el.querySelector('.shirt-color');
+                    let shirtDiscription = el.querySelector('.shirt-discription');
+                    let newPrice = el.querySelector('.newPrice');
+                    let oldPrice = el.querySelector('.oldPrice')
+                    let shirtName = el.querySelector('.shirt_name');
+                    let image = el.querySelector('.img-shirt')
+                    modalShirt.innerHTML = `
+                    <div class="row">
+
+                    <div class="col s12 m6">
+                    <img class="modal-img" src='${image.src}'> 
+                        </div>
+
+                    <div class="col s12 m6">
+                    
+                    <h5 class="shirtNameModal">${shirtName.textContent}</h5>
+                        
+                        <div class="shirt-brand"><b>Бренд:</b> ${shirtBrand.textContent}</div>
+                        <div class="shirt-discription"><b>Описания:</b> ${shirtDiscription.textContent}
+                        <div class="shirt-color">Цвет: ${shirtColor.textContent}</div>
+                        </div>
+                        <div class="price"><b>Цена:</b>
+                        <span class="oldPrice">${oldPrice.textContent}</span>
+                        <span class="newPriceModal">${newPrice.textContent}</span>
+                        </div>
+                    </div>
+                
+                </div>
+    
+                <div class="col s12">
+                    <form method="post" class="d-flex flex-column w-100" action="">
+                        <input placeholder="Имя*" type="text" name="name" required="">
+                        <input placeholder="Телефон*" type="text" name="phone" required="">
+                        <button class="grey darken-4 waves-effect waves-light btn" type="button">Заказать</button>
+                    </form>
+                </div>
+                    
+            `
+                })
+
+            })
+        }).then(() => {
+            let slick = $('.carusel-shirt');
+            slick.slick({
+                dots: true,
+                arrows: false,
+                lazyLoad: 'progressive',
+                slidesToShow: 1,
+            });
+        })
+
     let postImg = (array) => {
         let carousel = document.querySelectorAll('.carusel-shirt')
         array.forEach(src => {
             let img = document.createElement('img');
+            img.className = 'img-shirt';
             img.setAttribute('data-lazy', src);
             img.src = 'assets/img/placeholder.jpg';
             carousel.forEach(el => {
@@ -43,4 +106,3 @@ $(document).ready(function() {
     }
 
 });
-
